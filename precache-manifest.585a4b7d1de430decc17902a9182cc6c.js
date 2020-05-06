d1bce9009d982c1c5c144924b5f491b7f8e88830
Alekskarb <korbutowski@gmail.com>
2020-05-06 20:04:44 +0300

    Updates

---
+++ precache-manifest.585a4b7d1de430decc17902a9182cc6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501dd6139127256fd7ad8bfb16647293",
+    "url": "/Pop_tasks/index.html"
+  },
+  {
+    "revision": "da35dc0903e3f148687b",
+    "url": "/Pop_tasks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8bafc367249b26f3c613",
+    "url": "/Pop_tasks/static/js/2.a0c228d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pop_tasks/static/js/2.a0c228d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da35dc0903e3f148687b",
+    "url": "/Pop_tasks/static/js/main.6e30c69b.chunk.js"
+  },
+  {
+    "revision": "f9de790db1b732d25ef7",
+    "url": "/Pop_tasks/static/js/runtime-main.b38f529b.js"
+  }
+]);
