60ccf6489972b8c52370a4286e532ca7b9221eae
Alekskarb <korbutowski@gmail.com>
2020-05-09 20:29:08 +0300

    typing

--- src/2a_features/f1_auth/a1_login/Login.tsx
+++ src/2a_features/f1_auth/a1_login/Login.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 
-const Login = () => {
+
+const Login = (props:any) => {
+
+    let addText = (e: ChangeEvent<HTMLInputElement>) => {
+        props.addText(e);
+    };
     return <div>
         <h3> Login </h3>
+        <input type="text" onChange={addText} value={''}/>
+        <input type="email"/>
     </div>
 };
+
+
+
 export default Login;
 
--- src/2a_features/f1_auth/a1_login/loginReducer.ts
+++ src/2a_features/f1_auth/a1_login/loginReducer.ts
@@ -1,10 +1,21 @@
 export type StateType = Array<string>;
-export type ActionType = Array<string>;
+//  type ActionType = Array<string>;
+const CHANGE_TEXT = '2a_features/loginReducer/CHANGE_TEXT';
+export type ActionType = addTextType;
 
+let initialState: StateType = [];
 
-let initialState:StateType = [];
-
-const loginReducer = (state = initialState, action:ActionType) => {
-    return state;
+const loginReducer = (state: StateType = initialState, action: ActionType) => {
+    switch (action.type) {
+        case CHANGE_TEXT:
+            return {...state, text: action.text};
+        default:
+            return state;
+    }
 };
+
+export const addText = (text: string): addTextType => ({type: CHANGE_TEXT, text});
+type addTextType = {
+    type: typeof CHANGE_TEXT, text: string
+}
 export default loginReducer;
